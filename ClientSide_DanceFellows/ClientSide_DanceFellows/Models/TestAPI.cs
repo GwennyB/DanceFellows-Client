d581cb612dc6cd9eb511efccb79429036f874201
Jason Hiskey <jasonhi@crazyredhead.com>
2019-02-04 15:35:26 -0800

    Finally got the two stacks to talk to eachother

---
+++ ClientSide_DanceFellows/ClientSide_DanceFellows/Models/TestAPI.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ClientSide_DanceFellows.Models
+{
+    public class TestAPI
+    {
+        public string Name { get; set; }
+
+        TestAPI()
+        {
+            GetResponse();
+        }
+
+        static HttpClient client = new HttpClient();
+
+
+
+        public static async Task<TestAPI> GetResponse()
+        {
+            TestAPI test = new TestAPI();
+
+            string path = "http://localhost:56370/api/Competitors";
+
+            HttpResponseMessage responseMessage = await client.GetAsync(path);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                test.Name = await responseMessage.Content.ReadAsAsync<string>();
+            }
+            return test;
+        }
+
+
+    }
+}
