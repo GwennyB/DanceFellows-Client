ba18e856c42c9037d4cc106fafbc7d925087d07a
andrewquamme <andrewquamme@gmail.com>
2019-02-04 10:08:25 -0800

    build out Competition class

--- ClientSide_DanceFellows/ClientSide_DanceFellows/Models/Competition.cs
+++ ClientSide_DanceFellows/ClientSide_DanceFellows/Models/Competition.cs
@@ -7,5 +7,20 @@ namespace ClientSide_DanceFellows.Models
 {
     public class Competition
     {
+        public int ID { get; set; }
+        public CompType CompType { get; set; }
+        public Level Level { get; set; }
+
+        //navigation properties
+        public RegisteredCompetitor RegisteredCompetitor { get; set; }
+    }
+
+    public enum CompType
+    {
+        JackAndJill=0,
+        Strictly=1,
+        Classic=2,
+        Showcase=3,
+        RisingStar=4
     }
 }
