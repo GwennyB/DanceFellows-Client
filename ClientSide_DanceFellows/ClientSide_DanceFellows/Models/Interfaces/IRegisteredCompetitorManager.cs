3a981bb6e99a6462b182151f10bade971ae81e01
Jason Hiskey <jasonhi@crazyredhead.com>
2019-02-05 09:30:12 -0800

    Completed IRegisteredCompetitorManager

--- ClientSide_DanceFellows/ClientSide_DanceFellows/Models/Interfaces/IRegisteredCompetitorManager.cs
+++ ClientSide_DanceFellows/ClientSide_DanceFellows/Models/Interfaces/IRegisteredCompetitorManager.cs
@@ -7,5 +7,34 @@ namespace ClientSide_DanceFellows.Models.Interfaces
 {
     public interface IRegisteredCompetitorManager
     {
+        //Create RegisteredCompetitor
+        Task CreateRegisteredCompetitor(RegisteredCompetitor registeredCompetitor);
+
+        //Read An RegisteredCompetitor
+        Task<RegisteredCompetitor> GetRegisteredCompetitor(int id);
+
+        //Read ALL RegisteredCompetitor
+        Task<IEnumerable<RegisteredCompetitor>> GetRegisteredCompetitor();
+
+        //Update A RegisteredCompetitor
+        void UpdateRegisteredCompetitor(RegisteredCompetitor registeredCompetitor);
+
+        //Delete A RegisteredCompetitor
+        void DeleteRegisteredCompetitor(RegisteredCompetitor registeredCompetitor);
+
+        void DeleteRegisteredCompetitor(int id);
+
+        //Search RegisteredCompetitor
+        Task<IEnumerable<RegisteredCompetitor>> SearchRegisteredCompetitor(int bibNumber);
+
+        //Navagation Properties
+
+        //Adds RegisteredCompetitor to Participant nav props
+        Task AddParticipantAssociation(RegisteredCompetitor registeredCompetitor);
+
+        //Adds RegisteredCompetitor to Competition nav props
+        Task AddCompetitionAssociation(RegisteredCompetitor registeredCompetitor);
+
+        //TODO: Add submit to API Task
     }
 }
